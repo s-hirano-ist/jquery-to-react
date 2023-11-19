1259f56a1aaa3072530ea2c08ea499c6eb5bbaf9
s-hirano-ist <solucky0725@icloud.com>
2023-11-19 11:47:58 +0900

    feat: add prettier

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export type Book = {
+  id: number;
+  title: string;
+  author: string;
+  overview: string;
+  image: string;
+  reviews: Review[];
+};
+
+export type Review = {
+  id: number;
+  username: string;
+  comment: string;
+  like: number;
+};
