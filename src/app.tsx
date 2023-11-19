54995d91d2da8ff4e74776db5c4c6d50f1b4f1f4
s-hirano-ist <solucky0725@icloud.com>
2023-11-19 13:12:09 +0900

    feat: change codes to react

--- src/app.tsx
+++ src/app.tsx
@@ -1,50 +1,22 @@
-import $ from "jquery";
-import createBookListItem from "./createBookListItem";
-import createBookReview from "./createBookReview";
-import type { Book } from "./types";
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
+import Timeline from "./Timeline";
 
-const domNode = document.getElementById("react-root") as HTMLElement;
-const root = createRoot(domNode);
-root.render(<h1>Reactで描写する</h1>);
+export type Book = {
+  id: number;
+  title: string;
+  author: string;
+  overview: string;
+  image: string;
+  reviews: Review[];
+};
 
-$(function () {
-  $.ajax("http://localhost:1323/books").done(function (books: Book[]) {
-    books.forEach(book =>
-      $("#js-book-list").append($(createBookListItem(book))),
-    );
+export type Review = {
+  id: number;
+  username: string;
+  comment: string;
+  like: number;
+};
 
-    $(".js-toggle-review").on("click", function (event) {
-      var bookId = $(this).data("bookId");
-      $('.js-review[data-book-id="' + bookId + '"]').toggle("fast");
-
-      return false;
-    });
-
-    $(document).on("click", ".js-like", function (event) {
-      var likeCountElement = $(this).find(".js-like-count");
-      likeCountElement.text(likeCountElement.text() + 1);
-
-      return false;
-    });
-
-    $(document).on("submit", ".js-form", function (event) {
-      var bookId = $(this).data("bookId");
-      $.ajax({
-        url: "http://localhost:1323/reviews",
-        type: "post",
-        dataType: "json",
-        data: {
-          comment: $(event.currentTarget).find("textarea").val(),
-        },
-      }).done(function (review) {
-        $('.js-review[data-book-id="' + bookId + '"] > ul').append(
-          $(createBookReview(review)),
-        );
-      });
-
-      return false;
-    });
-  });
-});
+const root = document.getElementById("react-root");
+ReactDOM.render(<Timeline />, root);
